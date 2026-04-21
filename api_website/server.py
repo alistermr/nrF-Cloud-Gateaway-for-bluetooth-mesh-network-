e6129957e5c8569adadb8da8233225939d0bf822
alistermr <ali.amlashii@gmail.com>
2026-04-21 17:32:48 +0200

    Fix CORS for NTNU subdomain

--- api_website/server.py
+++ api_website/server.py
@@ -16,7 +16,7 @@ CORS(
     app,
     resources={r"/api/*": {
         "origins": [
-            "https://folk.ntnu.no",
+            "https://aliaml.folk.ntnu.no",
             "http://localhost:5500",
             "http://127.0.0.1:5500",
             "http://localhost:5000",
