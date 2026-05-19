4495f787a5b88c17d5a621873e7c0c7b6d8b4d01
YoPotato80 <mathias.schiotz@hotmail.no>
2026-05-19 02:14:05 +0200

    added comments and renamed folders for clarity

---
+++ Mesh_node/build_1/light/zephyr/include/generated/app_commit.h
@@ -0,0 +1,12 @@
+#ifndef _APP_COMMIT_H_
+#define _APP_COMMIT_H_
+
+/*  values come from cmake/version.cmake
+ * BUILD_COMMIT related  values will be 'git rev-parse',
+ * alternatively user defined BUILD_VERSION.
+ */
+
+#define APP_COMMIT                   d8887f6f32df
+#define APP_COMMIT_STRING            "d8887f6f32df"
+
+#endif /* _APP_COMMIT_H_ */
